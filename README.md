3b7e938e3d39d34f867756327f2df8950f436267
Mikhaerys <mikhaerys@gmail.com>
2024-10-14 18:33:36 -0500

    Added class that returns chatgpt response

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AI_Voice_ChatBot
-Simple project that captures user questions via voice, converts them to text, sends the query to the ChatGPT API for a response, and finally converts the response back to audio for playback. It enables seamless voice interaction with AI-powered responses.
+Simple project that captures user questions via voice, converts them to text, sends the query to a language model for a response, and finally converts the response back to audio for playback. It enables seamless voice interaction with AI-powered responses.
---
+++ chatgpt_responder.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+
+class ChatGPTResponder:
+    def __init__(self, api_key):
+        self.client = OpenAI(api_key=api_key)
+        self.messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ]
+
+    def get_response(self, user_input):
+        self.messages.append({"role": "user", "content": user_input})
+        response = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=self.messages
+        )
+        chatbot_response = response.choices[0].message["content"]
+        return chatbot_response
